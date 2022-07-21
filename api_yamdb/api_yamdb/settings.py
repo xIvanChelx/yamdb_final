5431f9e1aecd53288349f8f68e165e835fa2331c
Иван Писарев <ivan_chel@mail.ru>
2022-07-21 14:23:50 +0500

    settings edited

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "*")
 
 DEBUG = False
 
